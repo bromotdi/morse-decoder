796bd524c30175a948f2faad1557cf358077a515
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2023-02-24 01:30:31 +0300

    feat:  edit decode function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,20 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const decode = (expr) => {
+    let res = ""
+    for (let i = 0, j = 10; i < expr.length; i = i + 10, j = j + 10) {
+        let num = expr.slice(i, j), sign = ""
+        if (num === "**********") sign = sign + " "
+        else {
+            for (let a = 0, b = 2; a < 10; a = a + 2, b = b + 2) {
+                if(num.slice(a, b) === "10") sign = sign + "."
+                else if(num.slice(a, b) === "11") sign = sign + "-"
+            }
+        }
+        (sign === " ") ? res = res + " " : res = res + MORSE_TABLE[sign]
+    }
+    return res
 }
 
 module.exports = {
